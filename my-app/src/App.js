c020e6eeb077a443fddf2fedb896c8162478ec2a
Henry Feng <henryfeng12345@gmail.com>
2019-11-13 00:11:42 -0600

    clarify that app.js is a function component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+// function component: creating a component without using 
+// a class but instead a literal js function
 function App() {
   // return jsx
   return (
