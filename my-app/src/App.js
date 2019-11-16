9290cd9fabe16be2abdf0d3b57219b9d8eb6a2d5
Henry Feng <henryfeng12345@gmail.com>
2019-11-15 23:24:38 -0600

    add state to todos.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,31 +3,11 @@ import Todos from './components/Todos';
 
 import './App.css';
 
-state = {
-  todos: [
-    {
-      id: 1,
-      title: 'take out the trash',
-      completed: false
-    },
-    {
-      id: 2,
-      title: 'Dinner with friends',
-      completed: false
-    },
-    {
-      id: 3,
-      title: 'meeting',
-      completed: false
-    }
-  ]
-}
-
-
 // function component: creating a component without using 
 // a class but instead a literal js function
 function App() {
 
+  
   // return jsx
   return (
     <div className="App">
--- my-app/src/components/Todos.js
+++ my-app/src/components/Todos.js
@@ -1,7 +1,28 @@
 import React, {Component} from 'react';
 
 class Todos extends Component {
+    state = {
+        todos: [
+          {
+            id: 1,
+            title: 'take out the trash',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Dinner with friends',
+            completed: false
+          },
+          {
+            id: 3,
+            title: 'meeting',
+            completed: false
+          }
+        ]
+      }
+
     render() {
+        console.log(this.state);
         return (
             <div>
                 <h1>Todos</h1>
